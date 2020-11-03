b632014acec1b1ae73da9e0c90ec30b951732d7a
aurelianvoinea6 <aurash1991@hotmail.com>
2020-11-03 22:15:24 +0000

    async

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,16 +1,45 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
+export const Cardstarwars = () => {
+	const { store, actions } = useContext(Context);
 
-export const CardStarwars = () => {
 	return (
-		<Card style={{ width: "18rem" }}>
-			<Card.Header>StarWars</Card.Header>
-			<ListGroup variant="flush">
-				<ListGroup.Item>Cras justo odio</ListGroup.Item>
-				<ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-				<ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-			</ListGroup>
-		</Card>
+		<div className="card-columns">
+			{store.people.map((item, index) => {
+				return (
+					<div className="card" key={index}>
+						<img
+							className="card-image"
+							src="https://www.google.es/url?sa=i&url=https%3A%2F%2Fwww.muyinteresante.es%2Fcultura%2Farte-cultura%2Ftest%2Fcuanto-sabes-de-star-wars&psig=AOvVaw0aexuo_2WywgKwkEFsyQw2&ust=1604527411267000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCeisaw5-wCFQAAAAAdAAAAABAD"
+							alt="Star Wars"
+						/>
+						<div className="card-body">
+							<ul className="card-text">
+								<li>
+									<span>Name: {item.name}</span>
+								</li>
+								<li>
+									<span>Mass: {item.mass}</span>
+								</li>
+								<li>
+									<span>Gender: {item.gender}</span>
+								</li>
+								<li>
+									<span>Height: {item.height}</span>
+								</li>
+								<li>
+									<span>Skin: {item.skin_color}</span>
+								</li>
+								<li>
+									<span>Eye Color: {item.eye_color}</span>
+								</li>
+							</ul>
+						</div>
+						<div className="card-footer" />
+					</div>
+				);
+			})}
+		</div>
 	);
 };
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,85 +1,18 @@
+import { useEffect, useState } from "react";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			people: []
 		},
 		actions: {
-			getPeople: () => {
-				fetch("https://swapi.dev/api/people/", {})
-					.then(response => {
-						if (!response.ok) {
-							throw Error("I cannot acces people");
-						}
-
-						return response.json();
-					})
-					.then(responseAsJson => {
-						console.log(responseAsJson, "im done");
-						console.log("random");
-
-						getActions().setPeople(responseAsJson);
-					})
-					.catch(error => {
-						/checking errors/;
-						console.log(error);
-					});
-			},
-			setPeople: characters => {
-				console.log(characters, "random stuff");
-				let arr = [];
-				for (const key in characters) {
-					//console.log(key + ":" + characters[key], "en for in");
-					if (key === "results") arr = characters[key];
-				}
-				console.log(arr, "xd");
-
-				arr.map((character, index) => {
-					console.log(character[index], "test");
-					getStore().characters.push({
-						name: test[index].name
-					});
-				});
-			},
-			//-----------------------Aqui se guardan funciones que las guarda en store---------------------
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/* fetch("https://swapi.dev/api/planets/")
-					.then(response => {
-						if (response.ok) {
-							throw new Error("I can't load characters");
-						}
-						return response.json();
-					})
-					.then(responseAsJson => {
-						console.log(responseAsJson);
-
-						setStore(responseAsJson);
-					})
-					.catch(error => {
-						//manejo de errores
-						console.log(error);
-					}); */
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))  //Aqui metemos la API
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getPeople: async () => {
+				const data = await fetch("https://swapi.dev/api/people/");
+				const user = await data.json();
+				setStore({ people: user.results });
 			}
+
+			//Using the async function after a lot of faills :D
 		}
 	};
 };
